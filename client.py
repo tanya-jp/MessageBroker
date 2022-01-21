ec035aae720f3514c2cfd59506404e486cbd7410
tanya-jp <t_djavaherpour@yahoo.com>
2022-01-21 15:00:42 +0330

    "simple client and server"

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+
+# HOST = '127.0.0.1'
+PORT = 1011
+MESSAGE_LENGTH_SIZE = 1024
+ENCODING = 'utf-8'
+
+
+def main():
+    address = socket.gethostbyname(socket.gethostname())
+    SERVER_INFORMATION = (address, PORT)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(SERVER_INFORMATION)
+    # send_msg(s, "HELLO WORLD!")
+    # send_msg(s, "DISCONNECT")
+
+
+def send_msg(client, msg):
+    message = msg.encode(ENCODING)
+    msg_length = int(float(message))
+    msg_length = str(msg_length).encode(ENCODING)
+    msg_length += b' ' * (MESSAGE_LENGTH_SIZE - len(msg_length))
+
+    client.send(msg_length)
+    client.send(message)
+
+
+if __name__ == '__main__':
+    main()
